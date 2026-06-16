a77ec1ce1d31ce389057db0bfd5fc208bfa24cf8
AnderTy2106 <andersonty2106@gmail.com>
2026-06-16 11:10:05 -0500

    Inicio del proyecto

---
+++ src/main/java/com/perfumecollection/perfumemanagerapi/PerfumeManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.perfumecollection.perfumemanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfumeManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/perfumecollection/perfumemanagerapi/PerfumeManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfumecollection.perfumemanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
