4d1221b1293670d8b20142b7346ce1678f3fd066
AnderTy2106 <andersonty2106@gmail.com>
2026-06-26 19:46:31 -0500

    feat(model): creación del repositorio PerfumeRepository

---
+++ src/main/java/com/perfumecollection/perfumemanagerapi/repository/PerfumeRepository.java
@@ -0,0 +1,8 @@
+package com.perfumecollection.perfumemanagerapi.repository;
+
+import com.perfumecollection.perfumemanagerapi.model.Perfume;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfumeRepository extends JpaRepository<Perfume,Long> {
+
+}
